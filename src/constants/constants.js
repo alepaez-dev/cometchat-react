956f71a85b1a7361741f86f19179395011b5a44d
alepaez-dev <al3_hp@hotmail.com>
2022-01-31 19:59:04 -0600

    feat: cometchat initialization

---
+++ src/constants/constants.js
@@ -0,0 +1,4 @@
+// Constants file
+export const APP_ID = "2027089e1bfa6ac1"
+export const APP_REGION = "us"
+export const AUTH_KEY = "39c87f28ed78f11c2d85e3b1d19048a9f037ba0c"
--- src/index.js
+++ src/index.js
@@ -3,13 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as CONSTANTS from "./constants/constants";
+import { CometChat } from "@cometchat-pro/chat"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const appSetting = new CometChat.AppSettingsBuilder().subscribePresenceForAllUsers().setRegion(CONSTANTS.APP_REGION).build();
+CometChat.init(CONSTANTS.APP_ID, appSetting).then(
+  () => {
+    console.log("Initialization completed succesffully by Ale Paez");
+  // You can now call login function.
+  
+    ReactDOM.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+      document.getElementById('root')
+    );
+  }, error => {
+    console.log("Initialization failed with an error, AP", error);
+    // Check the reason for error and take appropiate action.
+  }
+)
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
